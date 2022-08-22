480a7f197df236090a6ed49b473cd79d8651128e
Iványi Kristóf <xivanyikristofx@gmail.com>
2022-08-22 16:13:07 +0200

    Fixing MS_SHOULD_BE_FINAL

--- src/main/java/my_app/Main.java
+++ src/main/java/my_app/Main.java
@@ -1,7 +1,7 @@
 package my_app;
 
 public class Main{
-	public static int SIDE_BUFFER_SIZE = 1024;
+	public static final int SIDE_BUFFER_SIZE = 1024;
 	
 	
 	public static void main(String[] args) {
