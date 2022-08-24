a63b5277900620b1c94e68563a8963081bb77911
Iványi Kristóf <xivanyikristofx@gmail.com>
2022-08-24 14:46:03 +0200

    Adding another warning

--- src/main/java/my_app/Main.java
+++ src/main/java/my_app/Main.java
@@ -3,6 +3,7 @@ package my_app;
 public class Main{
 	public static int POPULATION = 0;
 	public static final int SIDE_BUFFER_SIZE = 1024;
+	public static int AVERAGE_AGE = 0;
 	
 	
 	public static void main(String[] args) {
