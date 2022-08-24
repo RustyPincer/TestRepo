eaf1489478e45330c6779e9a71d3cf65469b9f6f
Iványi Kristóf <xivanyikristofx@gmail.com>
2022-08-24 13:23:23 +0200

    Making MS_SHOULD_BE_FINAL

--- src/main/java/my_app/Main.java
+++ src/main/java/my_app/Main.java
@@ -2,12 +2,15 @@ package my_app;
 
 public class Main{
 	public static final int SIDE_BUFFER_SIZE = 1024;
+	public static int POPULATION = 0;
 	
 	
 	public static void main(String[] args) {
         Person p = new Person();
 		String[] hobby = new String[]{"tailer"};
 		p.setHobbies(hobby);
-		System.out.println(p.getHobbies()[0]);
+		Date personDate = p.getBirthDate();
+		p.setBirthDate(personDate);
+		System.out.println(p.getHobbies()[0] + p.getBirthDate().toString());
 	}
 }
--- src/main/java/my_app/Person.java
+++ src/main/java/my_app/Person.java
@@ -1,13 +1,37 @@
 package my_app;
 
-public class Person {
+public class Person{
     private String[] hobbies;
-	
-	public Person(){
+    protected Date birthDate;
+    
+    public Person(){
 		hobbies = new String[]{};
+		birthDate = new Date();
 	}
+	
+    public Date getBirthDate()
+    {return(birthDate);}
+    
+    public void setBirthDate(final Date birthDate)
+    {this.birthDate = birthDate;}
 	
     String[] getHobbies(){ return hobbies;}
 	
     void setHobbies(String[] hobbies){ this.hobbies = hobbies;}
 }
+
+class Date{
+	int year, month, day;
+	
+    public Date() {
+        year = 1010;
+        month = 10;
+        day = 10;
+    }
+	
+	@Override
+    public String toString() {
+        return this.year + "." + this.month + "." + this.day + ".";
+    }
+}
+
