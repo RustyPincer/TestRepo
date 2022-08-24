6367faae527cd90033e081487df89d53b065b1ee
Iványi Kristóf <xivanyikristofx@gmail.com>
2022-08-24 13:25:33 +0200

    Moving MS_SHOULD_BE_FINAL warning one line up

--- src/main/java/my_app/Main.java
+++ src/main/java/my_app/Main.java
@@ -1,8 +1,8 @@
 package my_app;
 
 public class Main{
-	public static final int SIDE_BUFFER_SIZE = 1024;
 	public static int POPULATION = 0;
+	public static final int SIDE_BUFFER_SIZE = 1024;
 	
 	
 	public static void main(String[] args) {
