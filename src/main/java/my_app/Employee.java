c369751e0fc903b167bc8145ac5ba942d7fce9e1
Iványi Kristóf <xivanyikristofx@gmail.com>
2022-08-29 09:38:46 +0200

    Making 2 warnings

--- src/main/java/my_app/Employee.java
+++ src/main/java/my_app/Employee.java
@@ -1,8 +1,8 @@
 package my_app;
 
 public class Employee{
-	public static final int WORKING_HOURS = 8;
-	public static final int AVERAGE_BREAK = 20;
+	public static int WORKING_HOURS = 8;
+	public static int AVERAGE_BREAK = 20;
     private String[] hobbies;
     public Date birthDate;
     
